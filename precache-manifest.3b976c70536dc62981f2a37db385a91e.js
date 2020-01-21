78eedb9f383142c6457c011c00df3497416af1f1
LINN <mealiy62307@gmail.com>
2020-01-21 23:02:50 +0800

    Updates

---
+++ precache-manifest.3b976c70536dc62981f2a37db385a91e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be894236b5c104de395787681847935",
+    "url": "/twitchApi/index.html"
+  },
+  {
+    "revision": "e6aa78f1deb416cbdf5a",
+    "url": "/twitchApi/static/css/main.cb00de99.chunk.css"
+  },
+  {
+    "revision": "71a5fc9a313398f1b39f",
+    "url": "/twitchApi/static/js/2.3994f514.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/twitchApi/static/js/2.3994f514.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e6aa78f1deb416cbdf5a",
+    "url": "/twitchApi/static/js/main.c173cef8.chunk.js"
+  },
+  {
+    "revision": "bb31855208ff6a9abe24",
+    "url": "/twitchApi/static/js/runtime-main.5e1e8425.js"
+  },
+  {
+    "revision": "12580a7ae9b02b4f433b1b2448502781",
+    "url": "/twitchApi/static/media/bg.12580a7a.jpg"
+  }
+]);
